709c79fb8431405e1dc4cc34599761876daa8e60
Tim Storms <timstorms@gmail.com>
2017-08-19 21:33:37 -0400

    fixed missing reducers

--- src/routes/Roster/components/Roster.js
+++ src/routes/Roster/components/Roster.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+function renderPlayerRows(players) {
+  const rows = [];
+  players.foreach((player) => {
+    rows.push(
+      <div key={player.id} className="player-row">
+        <div className="col">
+          <input type="text" placeholder="First Name"/>
+        </div>
+        <div className="col">
+          <input type="text" placeholder="Last Name"/>
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">remove</button>
+        </div>
+      </div>);
+  });
+
+  return rows;
+}
+
 export const Roster = ({ roster }) => (
-  <div style={{ margin: '0 auto' }} >
+  <div style={{ margin: '0 auto' }} className="roster-container" >
     <h2>Roster</h2>
     <button className='btn btn-primary'>
       + Player
@@ -11,10 +31,35 @@ export const Roster = ({ roster }) => (
     <button className='btn btn-secondary'>
       Cancel
     </button>
+    <div className="roster">
+      <div className="roster-header">
+        <div className="col">First Name</div>
+        <div className="col">Last Name</div>
+        <div className="col">Action</div>
+      </div>
+      <div className="new-player player-row">
+        <div className="col">
+          <input type="text" />
+        </div>
+        <div className="col">
+          <input type="text" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </div>
+      <div className="players">
+        {renderPlayerRows}
+      </div>
+      <div className="buttons">
+        <button className="btn btn-primary">Apply</button>
+        <button className="btn btn-secondary">Cancel</button>
+      </div>
+    </div>
   </div>
 )
 Roster.propTypes = {
-  counter: PropTypes.array.isRequired
+  players: PropTypes.array
 }
 
 export default Roster
--- src/routes/Roster/modules/roster.js
+++ src/routes/Roster/modules/roster.js
@@ -9,7 +9,15 @@
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
+const ACTION_HANDLERS = {
+}
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
+const initialState = 0
+export default function rosterReducer (state = initialState, action) {
+  const handler = ACTION_HANDLERS[action.type]
+
+  return handler ? handler(state, action) : state
+}
